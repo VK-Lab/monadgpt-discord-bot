09c16dff071b8268fd02fd96cb6c2194677c298f
gaupoit <gaupoit@outlook.com>
2024-03-31 14:42:36 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+import os
+
+from src import bot
+from dotenv import load_dotenv
+from g4f.cookies import set_cookies
+
+
+if __name__ == '__main__':
+    set_cookies(".bing.com", {
+    "_U": str(os.getenv("BING_COOKIE"))
+    })
+    set_cookies("chat.openai.com", {
+    "access_token": str(os.getenv("OPENAI_TOKEN"))
+    })
+    set_cookies(".google.com", {
+    "__Secure-1PSID": str(os.getenv("GOOGLE_PSID"))
+    })
+    bot.run_discord_bot()
